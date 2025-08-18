a19bb56a386ee1f540ec019f7b80bc19237a0a67
afnan-hmd <91302955+afnan-hmd@users.noreply.github.com>
2025-08-18 10:07:37 +0400

    initial app

---
+++ creative-portfolio/src/components/header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const function Header () {
+  
+  const headerPages = [
+    "Home", "About", "Projects", "Contact"
+  ];
+
+  return (
+    <>
+      <navbar>
+        <p>meow</p>
+      </navbar>
+    </>
+  )
+};
+
+export Header;
